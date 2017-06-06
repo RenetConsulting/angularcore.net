a9df5eaf08d6edcec5ae966c99853f485fe3ecd2
Alexander Altotsky <alex@renetusa.com>
2017-06-06 10:12:05 -0700

    Added code covarage batch file

---
+++ Business/Application.Business.Test/RunCodeCoverage.bat
@@ -0,0 +1,18 @@
+﻿@echo off
+SET dotnet="C:/Program Files/dotnet/dotnet.exe"
+SET opencover=%USERPROFILE%\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe
+SET reportgenerator=%USERPROFILE%\.nuget\packages\reportgenerator\2.5.8\tools\ReportGenerator.exe
+
+SET targetargs="test"
+SET filter="+[*]Application.Business* -[*.Test]* -[xunit.*]* -[FluentValidation]*"
+SET coveragefile=Coverage.xml
+SET coveragedir=Coverage
+
+REM Run code coverage analysis
+"%opencover%"  -register:user -target:%dotnet% -output:%coveragefile% -targetargs:%targetargs% -filter:%filter% -skipautoprops -hideskipped:All -oldstyle 
+
+REM Generate the report
+"%reportgenerator%" -targetdir:%coveragedir% -reporttypes:Html;Badges -reports:%coveragefile% -verbosity:Error
+
+REM Open the report
+start "report" "%coveragedir%\index.htm"
