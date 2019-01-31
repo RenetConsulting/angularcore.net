afa448ebc465b80bcf9fb034b2034941c5a2b3c6
Michael Vasyliv <mihail.vasyliv.mail@gmail.com>
2019-01-31 19:38:37 +0200

    updated settings

--- UI/Application/ClientApp/src/app/consts/snack.bar.messager.options.ts
+++ UI/Application/ClientApp/src/app/consts/snack.bar.messager.options.ts
@@ -1,7 +1,7 @@
 import { MatSnackBarConfig } from "@angular/material/snack-bar";
 
 export const SNACK_BAR_MESSAGER_OPTIONS: MatSnackBarConfig = {
-    duration: 1500,
+    duration: 3500,
     direction: "ltr",
     horizontalPosition: "end",
     verticalPosition: "top"
