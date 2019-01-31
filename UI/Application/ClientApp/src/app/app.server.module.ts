1cd0f03c2f7278eee4d7df775cb364cb19a900f5
Michael Vasyliv <mihail.vasyliv.mail@gmail.com>
2019-01-31 16:21:41 +0200

    fixed a bug in prod build

--- UI/Application/ClientApp/src/app/app.server.module.ts
+++ UI/Application/ClientApp/src/app/app.server.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from "@angular/core";
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 import { ServerModule } from "@angular/platform-server";
 import { ModuleMapLoaderModule } from "@nguniversal/module-map-ngfactory-loader";
-import { AppModule } from "./app.module";
+import { AppBaseModule } from "./app.base.module";
 import { AppComponent } from "./components/app/app.component";
 
 @NgModule({
-    imports: [NoopAnimationsModule, AppModule, ServerModule, ModuleMapLoaderModule],
+    imports: [NoopAnimationsModule, AppBaseModule, ServerModule, ModuleMapLoaderModule],
     bootstrap: [AppComponent]
 })
 export class AppServerModule { }
