0cad6d2dd39747f7baa00956122cbfdfac9b2d29
Andrey Medvedev <andreygasparyan539@gmail.com>
2019-02-04 14:15:19 +0200

    Added unit test for send email

---
+++ Business/Application.Business.Test/CommunicationsFixture/MailClientFixture.cs
@@ -0,0 +1,27 @@
+﻿namespace Application.Business.Test.CommunicationsFixture
+{
+    using System.Threading.Tasks;
+    using Application.Business.Communications;
+    using Moq;
+    using Xunit;
+
+    public class MailClientFixture
+    {
+        [Fact]
+        public async Task SendEmailAsyncTest_EmailHasBeenSuccessfullySent()
+        {
+            const string sendGridKey = "E5E0BA8F-6DA5-43CA-ACF9-764384852C30";
+            const string emailFrom = "cemmy1993@gmail.com";
+            const string emailTo = "ToYourEmail@gmail.com";
+            const string subject = "Hello!";
+            const string messageBody = "My name is Andrey.";
+
+            Mock<SendGrid.SendGridClient> sendGridClientMock = new Mock<SendGrid.SendGridClient>(sendGridKey, null, null, null, null);
+
+            SendGrid.Response result = await MailClient.SendEmailAsync(sendGridClientMock.Object, emailFrom, emailTo, subject, messageBody)
+                .ConfigureAwait(false);
+
+            Assert.True(!Equals(result.Body, null));
+        }
+    }
+}
