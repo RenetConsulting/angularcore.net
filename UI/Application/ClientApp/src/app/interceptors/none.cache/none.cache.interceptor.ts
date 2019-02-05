e1661b346e1bf51473c004686741ceba5738c706
Michael Vasyliv <mihail.vasyliv.mail@gmail.com>
2019-02-05 13:08:48 +0200

    fix: errors in lint

--- UI/Application/ClientApp/src/app/interceptors/none.cache/none.cache.interceptor.ts
+++ UI/Application/ClientApp/src/app/interceptors/none.cache/none.cache.interceptor.ts
@@ -11,7 +11,7 @@ export class NoneCacheInterceptor implements HttpInterceptor {
         return handler.handle(request.clone({
             setHeaders: {
                 'Cache-Control': 'no-store, no-cache, must-revalidate',
-                'Pragma': 'no-cache'
+                Pragma: 'no-cache'
             }
         }));
     }
