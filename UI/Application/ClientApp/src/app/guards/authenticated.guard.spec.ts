0bab685403202a8b9d714c7eeafea14680842b36
Michael Vasyliv <mihail.vasyliv.mail@gmail.com>
2019-02-05 14:05:47 +0200

    added support e2e and jusmine tests

--- UI/Application/ClientApp/src/app/guards/authenticated.guard.spec.ts
+++ UI/Application/ClientApp/src/app/guards/authenticated.guard.spec.ts
@@ -1,15 +1,16 @@
-import { TestBed, async, inject } from '@angular/core/testing';
-
+import { inject, TestBed } from '@angular/core/testing';
 import { AuthenticatedGuard } from './authenticated.guard';
 
-describe('AuthenticatedGuard', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [AuthenticatedGuard]
-    });
-  });
 
-  it('should ...', inject([AuthenticatedGuard], (guard: AuthenticatedGuard) => {
-    expect(guard).toBeTruthy();
-  }));
+describe('AuthenticatedGuard', () => {
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [AuthenticatedGuard]
+        });
+    });
+
+    it('should ...', inject([AuthenticatedGuard], (guard: AuthenticatedGuard) => {
+        expect(guard).toBeTruthy();
+    }));
 });
