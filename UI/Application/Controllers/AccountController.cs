a8acd9c235cc656e12b88bc9725bec3a13458aa5
Andrey Medvedev <andreygasparyan539@gmail.com>
2019-02-18 08:33:10 +0200

    RT1-45 Added API to logout

--- UI/Application/Controllers/AccountController.cs
+++ UI/Application/Controllers/AccountController.cs
@@ -15,6 +15,7 @@ namespace Application.Controllers
     using Application.DataAccess.Entities;
     using Application.DataAccess.Enums;
     using Application.DataAccess.Repositories;
+    using Microsoft.AspNetCore.Authentication;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
@@ -25,11 +26,13 @@ namespace Application.Controllers
     public class AccountController : BaseController
     {
         private readonly ApplicationUserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
         private readonly IMailClient mailClient;
 
         public AccountController(
             IGlobalRepository repository,
             ApplicationUserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
             IOptions<AppSettings> appSettings,
             IMailClient mailClient,
             ILogger<AccountController> logger)
@@ -188,6 +191,23 @@ namespace Application.Controllers
             {
                 return this.BadRequest(ex.Message);
             }
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("SignOut")]
+        public async Task<IActionResult> SignOutAsync()
+        {
+            await this.HttpContext.SignOutAsync("Identity.Application")
+                .ConfigureAwait(false);
+
+            await this.HttpContext.SignOutAsync("Identity.External")
+                .ConfigureAwait(false);
+
+            await this.signInManager.SignOutAsync()
+                .ConfigureAwait(false);
+
+            return this.Ok();
         }
 
         internal async Task<ActionResult> SendEmailAsync(string emailTo, string subject, string message)
