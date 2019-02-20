8bd3e1481f14e95f93f71e6854d7d5fb0a6ecc0b
aaltotsky <alex@renetusa.com>
2019-02-20 11:24:06 -0800

    First attempt to add Capcha Validation

--- UI/Application/Controllers/AuthorizationController.cs
+++ UI/Application/Controllers/AuthorizationController.cs
@@ -5,6 +5,7 @@
 namespace Application.Controllers
 {
     using System.Linq;
+    using System.Net.Http;
     using System.Threading.Tasks;
     using Application.Business;
     using Application.DataAccess.Entities;
@@ -33,6 +34,34 @@ namespace Application.Controllers
 
         [HttpPost("~/connect/token")]
         [Produces("application/json")]
+        public async Task<IActionResult> Exchange([ModelBinder(typeof(OpenIddictMvcBinder))] OpenIdConnectRequest request, string hash, string captcha)
+        {
+            return await this.Exchange(request);
+
+            // For now ignore Captcha validation
+            if (!string.IsNullOrEmpty(hash) && !string.IsNullOrEmpty(captcha))
+            {
+                // Validate Captcha
+                using (HttpClient client = new HttpClient())
+                {
+                    string captchaValidate = string.Format("http://localhost:7071/api/CaptchaValidate?hash={0}&captcha={1}&clientId={2}", hash, captcha, "12345");
+
+                    HttpResponseMessage response = await client.GetAsync(captchaValidate);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return await this.Exchange(request);
+                    }
+                }
+            }
+
+            return this.BadRequest(new OpenIdConnectResponse
+            {
+                Error = OpenIdConnectConstants.Errors.InvalidRequest,
+                ErrorDescription = "Invalid Captcha."
+            });
+        }
+
         public async Task<IActionResult> Exchange([ModelBinder(typeof(OpenIddictMvcBinder))] OpenIdConnectRequest request)
         {
             if (request.IsPasswordGrantType())
