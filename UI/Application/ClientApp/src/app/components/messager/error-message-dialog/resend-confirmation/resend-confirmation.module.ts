ff83f9f1229f9d166c4d8e77849db9dc7c16ef8f
Michael Vasyliv <mihail.vasyliv.mail@gmail.com>
2019-03-03 18:28:08 +0200

    added raised button to ResendConfirmationModule

--- UI/Application/ClientApp/src/app/components/messager/error-message-dialog/resend-confirmation/resend-confirmation.module.ts
+++ UI/Application/ClientApp/src/app/components/messager/error-message-dialog/resend-confirmation/resend-confirmation.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { EffectsModule } from '@ngrx/effects';
 import { ResendConfirmationEffects } from './effects';
 import { ResendConfirmationComponent } from './resend-confirmation.component';
 
 const MODULES = [
     CommonModule,
+    MatButtonModule,
     EffectsModule.forFeature([ResendConfirmationEffects])
 ];
 
