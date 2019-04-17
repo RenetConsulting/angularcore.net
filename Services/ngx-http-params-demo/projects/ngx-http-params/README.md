cddd05dc8d7ed82461c128428cc9aca977a3647c
Michael Vasyliv <mihail.vasyliv.mail@gmail.com>
2019-04-17 14:56:53 +0300

    added ngx-http-params-demo

---
+++ Services/ngx-http-params-demo/projects/ngx-http-params/README.md
@@ -0,0 +1,24 @@
+# NgxHttpParams
+
+This library was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.2.0.
+
+## Code scaffolding
+
+Run `ng generate component component-name --project ngx-http-params` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module --project ngx-http-params`.
+> Note: Don't forget to add `--project ngx-http-params` or else it will be added to the default project in your `angular.json` file. 
+
+## Build
+
+Run `ng build ngx-http-params` to build the project. The build artifacts will be stored in the `dist/` directory.
+
+## Publishing
+
+After building your library with `ng build ngx-http-params`, go to the dist folder `cd dist/ngx-http-params` and run `npm publish`.
+
+## Running unit tests
+
+Run `ng test ngx-http-params` to execute the unit tests via [Karma](https://karma-runner.github.io).
+
+## Further help
+
+To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
---
+++ Services/ngx-http-params-demo/projects/ngx-http-params/src/lib/ngx-http-params.service.spec.ts
@@ -0,0 +1,85 @@
+import { TestBed } from '@angular/core/testing';
+import { NgxHttpParamsService } from './ngx-http-params.service';
+
+describe('NgxHttpParamsService', () => {
+
+  let service: NgxHttpParamsService;
+
+  beforeEach(() => {
+    service = TestBed.get(NgxHttpParamsService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+  describe('string case', () => {
+    it('string length > 0', () => {
+      const x = { param1: 'val1' };
+      expect(service.getParams(x).toString()).toEqual('param1=val1');
+    });
+    it('string length === 0', () => {
+      const x = { param1: '' };
+      expect(service.getParams(x).toString()).toEqual('param1=');
+    });
+  });
+  it('number case', () => {
+    const x = { param1: 'val1', param2: 2, };
+    expect(service.getParams(x).toString()).toEqual('param1=val1&param2=2');
+  });
+  it('boolean case', () => {
+    const x = { param1: 'val1', param2: 2, param3: true, param4: false };
+    expect(service.getParams(x).toString()).toEqual('param1=val1&param2=2&param3=true&param4=false');
+  });
+  it('array case', () => {
+    const x = {
+      param1: 'val1',
+      param2: 2,
+      numbers: [1, 2, 3, 4],
+      fruits: ['apple', 'pear', 'peach'],
+      vegetables: ['tomato', 'potato', 'cucumber'],
+    };
+    // tslint:disable-next-line
+    expect(service.getParams(x).toString()).toEqual('param1=val1&param2=2&numbers=1&numbers=2&numbers=3&numbers=4&fruits=apple&fruits=pear&fruits=peach&vegetables=tomato&vegetables=potato&vegetables=cucumber');
+  });
+  describe('object case', () => {
+    it('with data', () => {
+      const x = {
+        param1: 'val1',
+        param2: 2,
+        numbers: [1, 2, 3, 4],
+        fruits: ['apple', 'pear', 'peach'],
+        vegetables: { tomato: 1, potato: 0.5, cucumber: 4 },
+      };
+      // tslint:disable-next-line
+      expect(service.getParams(x).toString()).toEqual('param1=val1&param2=2&numbers=1&numbers=2&numbers=3&numbers=4&fruits=apple&fruits=pear&fruits=peach&vegetables.tomato=1&vegetables.potato=0.5&vegetables.cucumber=4');
+    });
+    it('with null', () => {
+      const x = {
+        param1: 'val1',
+        param2: 2,
+        numbers: [1, 2, 3, 4],
+        fruits: null,
+        vegetables: { tomato: 1, potato: 0.5, cucumber: 4 },
+      };
+      // tslint:disable-next-line
+      expect(service.getParams(x).toString()).toEqual('param1=val1&param2=2&numbers=1&numbers=2&numbers=3&numbers=4&vegetables.tomato=1&vegetables.potato=0.5&vegetables.cucumber=4');
+    });
+  });
+  it('case with undefined', () => {
+    const x = {
+      param1: 'val1',
+      param2: 2,
+      numbers: [1, 2, 3, 4],
+      fruits: undefined,
+      vegetables: { tomato: 1, potato: 0.5, cucumber: 4 },
+    };
+    // tslint:disable-next-line
+    expect(service.getParams(x).toString()).toEqual('param1=val1&param2=2&numbers=1&numbers=2&numbers=3&numbers=4&vegetables.tomato=1&vegetables.potato=0.5&vegetables.cucumber=4');
+  });
+  it('case with null', () => {
+    expect(service.getParams(null).toString()).toEqual('');
+  });
+  it('case with undefined', () => {
+    expect(service.getParams(undefined).toString()).toEqual('');
+  });
+});
