e8361245f6902acfe12e6e03e87ff179a140a7ca
Michael Vasyliv <mihail.vasyliv.mail@gmail.com>
2019-04-17 14:57:48 +0300

    removed ngx-utils-demo

--- Services/ngx-utils-demo/projects/ngx-utils/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ngx-utils
- */
-
-export * from './lib/ngx-http-params.service';
-export * from './lib/ngx-validator/ngx-validator.directive';
-export * from './lib/ngx-validator/ngx-validator.module';
