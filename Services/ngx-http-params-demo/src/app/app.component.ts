331e79898aac3d776653fc267cc770b42e990279
Michael Vasyliv <mihail.vasyliv.mail@gmail.com>
2019-04-17 15:00:04 +0300

    updated demo

--- Services/ngx-http-params-demo/src/app/app.component.ts
+++ Services/ngx-http-params-demo/src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { NgxHttpParamsService } from 'projects/ngx-http-params/src/public-api';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngx-http-params-demo';
+
+    constructor(
+        private util: NgxHttpParamsService,
+        private http: HttpClient
+    ) { }
+
+    ngOnInit(): void {
+        this.http.get('https://httpbin.org/get', {
+            params: this.util.getParams({
+                param1: 'val1',
+                param2: 2,
+                numbers: [1, 2, 3, 4],
+                fruits: ['apple', 'pear', 'peach'],
+                vegetables: { tomato: 1, potato: 0.5, cucumber: 4 },
+            })
+        }).subscribe(console.log);
+    }
 }
--- Services/ngx-http-params-demo/src/app/app.module.ts
+++ Services/ngx-http-params-demo/src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
