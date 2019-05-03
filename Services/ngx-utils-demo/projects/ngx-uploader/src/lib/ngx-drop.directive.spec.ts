bcc1075eb17cf329a5a222a1b22193db68140e3b
Michael Vasyliv <mihail.vasyliv.mail@gmail.com>
2019-05-03 17:02:20 +0300

    added tests to ngx-uploader (drop, select, base, utils)

---
+++ Services/ngx-utils-demo/projects/ngx-uploader/src/lib/ngx-drop.directive.spec.ts
@@ -0,0 +1,58 @@
+import { ElementRef, EventEmitter, Renderer2 } from '@angular/core';
+import { NgxDropDirective } from './ngx-drop.directive';
+import { NgxUploaderBase } from './ngx-uploader.base';
+
+describe('NgxDropDirective', () => {
+
+    let directive: NgxDropDirective;
+
+    const elementRef = new ElementRef(null);
+    let renderer: jasmine.SpyObj<Renderer2>;
+    let event: jasmine.SpyObj<Event> & { [key: string]: any };
+    const files = [];
+
+    beforeEach(() => {
+        event = jasmine.createSpyObj<Event>('Event', ['stopPropagation', 'preventDefault']);
+        event.dataTransfer = { files };
+        renderer = jasmine.createSpyObj<Renderer2>('Renderer2', ['removeClass', 'addClass']);
+        directive = new NgxDropDirective(elementRef, renderer);
+    });
+
+    it('emitter instanceof NgxUploaderBase', () => {
+        expect(directive instanceof NgxUploaderBase).toEqual(true);
+    });
+    it('dropClass', () => {
+        expect(directive.dropClass).toEqual('drop');
+    });
+    it('emitter instanceof EventEmitter', () => {
+        expect(directive.emitter instanceof EventEmitter).toEqual(true);
+    });
+    it('element', () => {
+        expect(directive.element).toEqual(elementRef.nativeElement);
+    });
+    it('drop', () => {
+        spyOn(directive, 'stopEvent');
+        spyOn(directive, 'emit');
+        directive.drop(event);
+        expect(directive.stopEvent).toHaveBeenCalledWith(event);
+        expect(renderer.removeClass).toHaveBeenCalledWith(directive.element, directive.dropClass);
+        expect(directive.emit).toHaveBeenCalledWith(files);
+    });
+    it('dragenter', () => {
+        spyOn(directive, 'stopEvent');
+        directive.dragenter(event);
+        expect(directive.stopEvent).toHaveBeenCalledWith(event);
+        expect(renderer.addClass).toHaveBeenCalledWith(directive.element, directive.dropClass);
+    });
+    it('dragleave', () => {
+        spyOn(directive, 'stopEvent');
+        directive.dragleave(event);
+        expect(directive.stopEvent).toHaveBeenCalledWith(event);
+        expect(renderer.removeClass).toHaveBeenCalledWith(directive.element, directive.dropClass);
+    });
+    it('stopEvent', () => {
+        directive.stopEvent(event);
+        expect(event.stopPropagation).toHaveBeenCalled();
+        expect(event.preventDefault).toHaveBeenCalled();
+    });
+});
