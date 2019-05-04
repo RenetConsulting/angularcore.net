8efeb90a439c80a058fc72074578fb8b8d0365b5
Michael Vasyliv <mihail.vasyliv.mail@gmail.com>
2019-05-04 17:53:06 +0300

    created mismatchPasswordValidator

--- UI/Application/ClientApp/src/app/components/account/change-password/change-password.component.ts
+++ UI/Application/ClientApp/src/app/components/account/change-password/change-password.component.ts
@@ -1,10 +1,10 @@
 import { ChangeDetectionStrategy, Component, Inject, OnDestroy, OnInit } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ValidationErrors } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { share } from 'rxjs/operators';
 import { PASSWORD_VALIDATORS } from '~/consts/password.validators';
-import { MessagesType } from '~/enums/messages.type';
 import { IChangePassword } from '~/interfaces/change-password';
+import { mismatchPasswordValidator } from '~/validators/mismatch-password.validator';
 import { ChangePassword, ResetError } from './actions';
 import { ChangePasswordStore } from './reducer';
 import { selectChangePasswordError } from './selectors';
@@ -31,16 +31,11 @@ export class ChangePasswordComponent implements OnInit, OnDestroy {
         this.store.dispatch(new ResetError());
     }
 
-    matchPasswordValidator = (control: AbstractControl): ValidationErrors | null => {
-        return control.value === (this.formGroup && this.formGroup.controls.password.value) ? null
-            : { errorMessage: MessagesType.passwordsDoNotMatch };
-    }
-
     setFormGroup = (): void => {
         this.formGroup = new FormGroup({
             oldPassword: new FormControl('', [...PASSWORD_VALIDATORS]),
             password: new FormControl('', [...PASSWORD_VALIDATORS]),
-            confirmPassword: new FormControl('', [...PASSWORD_VALIDATORS, this.matchPasswordValidator]),
+            confirmPassword: new FormControl('', [...PASSWORD_VALIDATORS, mismatchPasswordValidator()]),
         } as MapPick<IChangePassword, keyof IChangePassword, FormControl>);
     }
 
