6bddc0ac1e7c1ea524a6c9dcace16b124fd698d3
Michael Vasyliv <mihail.vasyliv.mail@gmail.com>
2019-05-13 18:29:31 +0300

    fixed bugs in tests

--- UI/Application/ClientApp/src/app/services/person/person.service.spec.ts
+++ UI/Application/ClientApp/src/app/services/person/person.service.spec.ts
@@ -1,12 +1,24 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
 import { PersonService } from './person.service';
 
 describe('PersonService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: PersonService = TestBed.get(PersonService);
-    expect(service).toBeTruthy();
-  });
+    let service: PersonService;
+
+    let controller: HttpTestingController;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+        })
+        service = TestBed.get(PersonService);
+        controller = TestBed.get(HttpTestingController);
+    });
+
+    afterEach(() => controller.verify());
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
