4f1716d16b2f00c6c07a0d8b5df2920f6288a865
Michael Vasyliv <mihail.vasyliv.mail@gmail.com>
2019-05-27 15:07:36 +0300

    updated peerDependencies in @renet-consulting/ngx-messenger

--- Services/ngx-utils-demo/src/app/storage/storage.component.ts
+++ Services/ngx-utils-demo/src/app/storage/storage.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { StorageService } from 'projects/storage/src/public-api';
-//import { StorageService } from '@renet-consulting/storage';
+// import { StorageService } from '@renet-consulting/storage';
 
 @Component({
     selector: 'app-storage',
@@ -13,7 +13,7 @@ export class StorageComponent implements OnInit {
     formGroup = new FormGroup({
         key: new FormControl(),
         value: new FormControl()
-    })
+    });
 
     constructor(
         private storage: StorageService
