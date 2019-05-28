d815effcfec67aa0379a61df9a01195fe49a0d7d
Michael Vasyliv <mihail.vasyliv.mail@gmail.com>
2019-05-28 15:51:39 +0300

    added tests to resetPasswordReducer

---
+++ UI/Application/ClientApp/src/app/components/account/reset-password/reducer.spec.ts
@@ -0,0 +1,23 @@
+import { Reset } from '~/actions/root.actions';
+import { ResetError, ResetPasswordError, ResetPasswordRequest } from './actions';
+import { resetPasswordReducer } from './reducer';
+
+describe('resetPasswordReducer', () => {
+
+    it('RESET_PASSWORD_REQUEST', () => {
+        expect(resetPasswordReducer({}, new ResetPasswordRequest(null))).toEqual({});
+    });
+    it('RESET_PASSWORD_ERROR', () => {
+        const error = { error: 'bob' };
+        expect(resetPasswordReducer({}, new ResetPasswordError(error))).toEqual({ error });
+    });
+    it('RESET_PASSWORD_ERROR', () => {
+        expect(resetPasswordReducer({}, new ResetError())).toEqual({ error: null });
+    });
+    it('RESET_PASSWORD_ERROR', () => {
+        expect(resetPasswordReducer({}, new Reset())).toEqual({});
+    });
+    it('null', () => {
+        expect(resetPasswordReducer({}, { type: null })).toEqual({});
+    });
+});
