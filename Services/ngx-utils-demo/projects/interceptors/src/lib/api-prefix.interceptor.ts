a1c36390bf7e010a754832be69ab11af85da93c0
Michael Vasyliv <mihail.vasyliv.mail@gmail.com>
2019-05-29 17:05:20 +0300

    added lib interceptors

--- Services/ngx-utils-demo/projects/interceptors/src/lib/api-prefix.interceptor.ts
+++ Services/ngx-utils-demo/projects/interceptors/src/lib/api-prefix.interceptor.ts
@@ -1,20 +1,17 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { BASE_URL } from '~/tokens/base-url.token';
 
-@Injectable({
-    providedIn: 'root'
-})
+@Injectable()
 export class ApiPrefixInterceptor implements HttpInterceptor {
 
     constructor(
-        @Inject(BASE_URL) private baseUrl: string
+        private prefix: string
     ) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         if (!/^(http|https):/i.test(request.url)) {
-            request = request.clone({ url: `${this.baseUrl}${request.url}` });
+            request = request.clone({ url: `${this.prefix}${request.url}` });
         }
         return next.handle(request);
     }
---
+++ Services/ngx-utils-demo/projects/interceptors/src/lib/none-cache.interceptor.ts
@@ -0,0 +1,18 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class NoneCacheInterceptor implements HttpInterceptor {
+
+    constructor() { }
+
+    intercept(request: HttpRequest<any>, handler: HttpHandler): Observable<HttpEvent<any>> {
+        return handler.handle(request.clone({
+            setHeaders: {
+                'Cache-Control': 'no-store, no-cache, must-revalidate',
+                Pragma: 'no-cache'
+            }
+        }));
+    }
+}
