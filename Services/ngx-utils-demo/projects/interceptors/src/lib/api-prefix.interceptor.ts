2e2aca82969d81f62e07c4af950d7d0f2e51b583
Michael Vasyliv <mihail.vasyliv.mail@gmail.com>
2019-05-29 17:14:52 +0300

    refactor: omit Injectable in @renet-consulting/interceptors

--- Services/ngx-utils-demo/projects/interceptors/src/lib/api-prefix.interceptor.ts
+++ Services/ngx-utils-demo/projects/interceptors/src/lib/api-prefix.interceptor.ts
@@ -1,8 +1,6 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-@Injectable()
 export class ApiPrefixInterceptor implements HttpInterceptor {
 
     constructor(
