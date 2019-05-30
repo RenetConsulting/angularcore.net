b6a5c28603a08c4228268ca97c289c7856b1428d
Michael Vasyliv <mihail.vasyliv.mail@gmail.com>
2019-05-31 01:33:48 +0300

    refactor: omit IConnectToken in auth

--- Services/ngx-utils-demo/projects/auth/src/lib/connect-token.ts
+++
@@ -1,8 +0,0 @@
-/** the interface is based on {@link OpenIdConnectRequest} */
-export interface IConnectToken {
-    grant_type: string;
-    scope: string;
-    username?: string;
-    password?: string;
-    refresh_token?: string;
-}
--- Services/ngx-utils-demo/projects/auth/src/lib/token.ts
+++ Services/ngx-utils-demo/projects/auth/src/lib/token.ts
@@ -1,9 +1,13 @@
 /** the interface is based on {@link OpenIdConnectRequest} */
 export interface IToken {
-    access_token: string;
-    refresh_token: string;
-    expires_in: number;
+    access_token?: string;
+    refresh_token?: string;
+    expires_in?: number;
+    token_type?: string;
+    grant_type?: string;
+    scope?: string;
+    username?: string;
+    password?: string;
     /** custom property that check time of authorization */
-    expired_at: string;
-    token_type: string;
+    expired_at?: string;
 }
