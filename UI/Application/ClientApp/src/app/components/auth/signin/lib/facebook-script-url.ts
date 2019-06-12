d8276082e1fd505054db7eefe0c6377cea97d738
Michael Vasyliv <mihail.vasyliv.mail@gmail.com>
2019-06-12 18:31:10 +0300

    updated signin component

--- UI/Application/ClientApp/src/app/components/auth/signin/lib/facebook-script-url.ts
+++
@@ -1,6 +0,0 @@
-import { InjectionToken } from '@angular/core';
-
-export const FACEBOOK_SCRIPT_URL = new InjectionToken<string>('FACEBOOK_SCRIPT_URL', {
-    providedIn: 'root',
-    factory: () => '//connect.facebook.net/en_US/sdk.js'
-});
