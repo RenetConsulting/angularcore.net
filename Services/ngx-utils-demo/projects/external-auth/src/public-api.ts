412c2b6d65d3c9048627b8a8ab312f2a8eb880b5
Michael Vasyliv <mihail.vasyliv.mail@gmail.com>
2019-06-12 18:24:53 +0300

    created package @renet-consulting/external-auth

---
+++ Services/ngx-utils-demo/projects/external-auth/src/public-api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of external-auth
+ */
+
+export * from './lib/external-auth.base';
+export * from './lib/facebook-script-url';
+export * from './lib/facebook-signin/facebook-signin.module';
+export * from './lib/google-script-url';
+export * from './lib/google-signin/google-signin.module';
