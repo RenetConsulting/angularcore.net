24489a17d1b3d6f8013bc93638b34a906ad43f8b
Michael Vasyliv <mihail.vasyliv.mail@gmail.com>
2019-06-27 14:34:02 +0300

    upated NgxMatInputComponent

--- Services/ngx-utils-demo/projects/ngx-uploader/src/lib/utils.ts
+++ Services/ngx-utils-demo/projects/ngx-uploader/src/lib/utils.ts
@@ -17,6 +17,12 @@ export const checkFileTypes = (file: File, accept: string): boolean => {
 export const filterFiles = (files: FileList | Array<File>, accept: string) =>
     Array.from(files).filter(x => checkFileTypes(x, accept));
 
+/**
+ * @param files FileList a list of files to filter
+ * @param accept string allowed file types, split by comma
+ * @param multiple boolean whether the method has to return one file or multiple
+ * @returns Array<File> validated files, a list may contain one file or multiple
+ */
 export const filterFileList = (files: FileList, accept: string, multiple: boolean) =>
     multiple ? filterFiles(files, accept) : filterFiles([files[0]], accept);
 
