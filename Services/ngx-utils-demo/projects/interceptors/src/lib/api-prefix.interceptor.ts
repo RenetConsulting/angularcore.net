c20120f60b4e89f7d1f4798167574bfe7543bd95
Michael Vasyliv <mihail.vasyliv.mail@gmail.com>
2019-07-08 11:43:45 +0300

    fix: build in ApiPrefixInterceptor

--- Services/ngx-utils-demo/projects/interceptors/src/lib/api-prefix.interceptor.ts
+++ Services/ngx-utils-demo/projects/interceptors/src/lib/api-prefix.interceptor.ts
@@ -1,10 +1,6 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-@Injectable({
-    providedIn: 'root'
-})
 export class ApiPrefixInterceptor implements HttpInterceptor {
 
     constructor(
