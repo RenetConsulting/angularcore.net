b2b2daf3ddf96847cdbe279488b6a6ab715131df
Michael Vasyliv <mihail.vasyliv.mail@gmail.com>
2019-08-04 01:00:15 +0300

    feat: adding default title to @renet-consulting/title-strategy

--- Services/ngx-utils-demo/projects/title-strategy/src/lib/title-strategy.service.ts
+++ Services/ngx-utils-demo/projects/title-strategy/src/lib/title-strategy.service.ts
@@ -1,6 +1,7 @@
-import { Inject, Injectable } from '@angular/core';
+import { Inject, Injectable, Optional } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Data } from '@angular/router';
+import { DEFAULT_TITLE_TOKEN } from './default-title.token';
 
 @Injectable({
     providedIn: 'root'
@@ -8,10 +9,14 @@ import { Data } from '@angular/router';
 export class TitleStrategyService {
 
     constructor(
-        @Inject(Title) private title: Title
+        @Inject(Title) private title: Title,
+        @Inject(DEFAULT_TITLE_TOKEN) @Optional() private defaultTitle: string
     ) { }
 
     set = (value: Data) => {
-        this.title.setTitle(value.title);
+        const title = value.title || this.defaultTitle;
+        if (title) {
+            this.title.setTitle(title);
+        }
     }
 }
