3aa3f601aa191c0e522fe7f8b865095bedc5d960
Michael Vasyliv <mihail.vasyliv.mail@gmail.com>
2019-08-04 16:20:54 +0300

    refactor: renamed folder name to angular

--- UI/Application/Startup.cs
+++ UI/Application/Startup.cs
@@ -102,7 +102,7 @@ namespace Application
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp/dist/angular7";
+                configuration.RootPath = "ClientApp/dist/angular";
             });
 
             services.AddDbContext<DataContext>(options =>
