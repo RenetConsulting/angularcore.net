f66422195b7323402e0eb837a1cb68ecbefe8dbf
Andrey Medvedev <andreygasparyan539@gmail.com>
2019-08-12 14:27:49 +0300

    blog - added migration

---
+++ UI/Application/Migrations/20190812112729_UpdateUserIdMigration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Application.Migrations
+{
+    public partial class UpdateUserIdMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "FileStorages",
+                maxLength: 450,
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FileStorages_UserId",
+                table: "FileStorages",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FileStorages_AspNetUsers_UserId",
+                table: "FileStorages",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FileStorages_AspNetUsers_UserId",
+                table: "FileStorages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FileStorages_UserId",
+                table: "FileStorages");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "FileStorages");
+        }
+    }
+}
