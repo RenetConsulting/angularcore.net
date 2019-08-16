fc3225fa54075cabdd051338e79e443fed37563c
Michael Vasyliv <mihail.vasyliv.mail@gmail.com>
2019-08-16 18:10:31 +0300

    pin @renet-consulting/editor to 8.0.3

--- Services/ngx-utils-demo/src/app/editor/editor.module.ts
+++ Services/ngx-utils-demo/src/app/editor/editor.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
 import { EditorModule as Editor } from 'projects/editor/src/public-api';
 import { EditorRoutingModule } from './editor-routing.module';
 import { EditorComponent } from './editor.component';
@@ -11,7 +12,8 @@ import { EditorComponent } from './editor.component';
         CommonModule,
         EditorRoutingModule,
         ReactiveFormsModule,
-        Editor
+        Editor,
+        MatCardModule
     ]
 })
 export class EditorModule { }
