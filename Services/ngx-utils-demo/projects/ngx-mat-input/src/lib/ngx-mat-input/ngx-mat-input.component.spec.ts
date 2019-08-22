475957d4d4216e963966795b0e49bf90a6048d43
Michael Vasyliv <mihail.vasyliv.mail@gmail.com>
2019-08-22 16:27:08 +0300

    refactor: ngx-mat-input settings attr name

--- Services/ngx-utils-demo/projects/ngx-mat-input/src/lib/ngx-mat-input/ngx-mat-input.component.spec.ts
+++ Services/ngx-utils-demo/projects/ngx-mat-input/src/lib/ngx-mat-input/ngx-mat-input.component.spec.ts
@@ -1,11 +1,21 @@
+import { EventEmitter, Renderer2 } from '@angular/core';
+import { FormGroupDirective } from '@angular/forms';
 import { NgxMatInputComponent } from './ngx-mat-input.component';
 
 describe('NgxMatInputComponent', () => {
 
     let component: NgxMatInputComponent;
 
+    let formGroup: FormGroupDirective;
+    let renderer: jasmine.SpyObj<Renderer2>;
+
     beforeEach(() => {
-        component = new NgxMatInputComponent(null, null);
+
+        renderer = jasmine.createSpyObj<Renderer2>('Renderer2', ['setAttribute', 'removeAttribute']);
+
+        component = new NgxMatInputComponent(renderer, null, null);
+
+        formGroup = {} as FormGroupDirective;
     });
 
     it('should create', () => {
@@ -16,5 +26,27 @@ describe('NgxMatInputComponent', () => {
     });
     it('type', () => {
         expect(component.type).toEqual('text');
+    });
+    it('ngOnChanges', () => {
+        spyOn(component, 'setErrorsState');
+        spyOn(component, 'setHintState');
+        spyOn(component, 'setRequired');
+        spyOn(component, 'setMaxlength');
+        spyOn(component, 'toggleAttribute');
+        component.ngOnChanges({ errors: {}, name: {} });
+        expect(component.setErrorsState).toHaveBeenCalled();
+        expect(component.setHintState).toHaveBeenCalled();
+        expect(component.setRequired).toHaveBeenCalled();
+        expect(component.setMaxlength).toHaveBeenCalled();
+        expect(component.toggleAttribute).toHaveBeenCalled();
+    });
+    it('ngOnInit', () => {
+        spyOn(component, 'setError');
+        spyOn(component, 'toggleAttribute');
+        formGroup.ngSubmit = jasmine.createSpyObj<EventEmitter<any>>('EventEmitter', ['subscribe']);
+        component.ngOnInit();
+        expect(component.setError).toHaveBeenCalledWith(null);
+        expect(component.toggleAttribute).toHaveBeenCalled();
+        component.ngOnDestroy();
     });
 });
