ef7d8a926e8c35ea8075858f9a05d6f467e2ec09
aaltotsky <alex@renetusa.com>
2020-04-22 17:32:11 -0700

    Run ng update @angular/core @angular/cli --force

--- UI/Application/ClientApp/src/app/consts/core-captcha-options.ts
+++ UI/Application/ClientApp/src/app/consts/core-captcha-options.ts
@@ -1,5 +1,7 @@
 import { ICoreCaptchaOptions } from '@renet-consulting/core-captcha';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class CoreCaptchaOptions implements ICoreCaptchaOptions {
 
     readonly height = 80;
