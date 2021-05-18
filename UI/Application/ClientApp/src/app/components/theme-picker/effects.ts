22f33b8ce3dc8d9eec6e5aef3553398582afe93a
romaschka1 <evrikaa6@gmail.com>
2021-05-18 12:58:45 +0300

    fix ie11 theme errors

--- UI/Application/ClientApp/src/app/components/theme-picker/effects.ts
+++ UI/Application/ClientApp/src/app/components/theme-picker/effects.ts
@@ -1,8 +1,10 @@
+import { DOCUMENT } from '@angular/common';
 import { Inject, Injectable } from '@angular/core';
+
 import { Actions, Effect, ofType, ROOT_EFFECTS_INIT } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
-import { NgxLinkStylesheetService } from '@renet-consulting/ngx-link-stylesheet';
 import { StorageService } from '@renet-consulting/storage';
+
 import { map, tap, withLatestFrom } from 'rxjs/operators';
 import { RootStore } from '~/reducers';
 import { SetTheme } from './actions';
@@ -12,28 +14,37 @@ import { ThemeTypes } from './types';
 @Injectable()
 export class ThemeEffects {
 
-    readonly key = 'theme';
-    readonly cssClass = 'theme-picker';
+	readonly key = 'theme';
+	readonly cssClass = 'theme-picker';
 
-    constructor(
-        @Inject(Actions) private actions: Actions,
-        @Inject(Store) private store: Store<RootStore>,
-        @Inject(StorageService) private storageService: StorageService,
-        @Inject(NgxLinkStylesheetService) private stylesheet: NgxLinkStylesheetService
-    ) { }
+	constructor(
+		@Inject(Actions) private actions: Actions,
+		@Inject(Store) private store: Store<RootStore>,
+		@Inject(StorageService) private storageService: StorageService,
+		@Inject(DOCUMENT) private document: Document
+	) { }
 
-    @Effect() init = this.actions.pipe(
-        ofType(ROOT_EFFECTS_INIT),
-        map(() => this.storageService.get(this.key)),
-        withLatestFrom(this.store.select(selectThemes)),
-        map(([selected, themes]) => selected ? selected : themes.find(i => i.isDefault)),
-        map(i => new SetTheme(i)),
-    );
+	@Effect() init = this.actions.pipe(
+		ofType(ROOT_EFFECTS_INIT),
+		map(() => this.storageService.get(this.key)),
+		withLatestFrom(this.store.select(selectThemes)),
+		map(([selected, themes]) => selected ? selected : themes.find(i => i.isDefault)),
+		map(i => new SetTheme(i)),
+	);
 
-    @Effect({ dispatch: false }) setTheme = this.actions.pipe(
-        ofType<SetTheme>(ThemeTypes.SET_THEME),
-        map(a => a.payload),
-        tap(i => i.isDefault ? this.stylesheet.delete(this.cssClass) : this.stylesheet.update(this.cssClass, `assets/${i.name}.css`)),
-        tap(x => this.storageService.set(this.key, x)),
-    );
+	@Effect({ dispatch: false }) setTheme = this.actions.pipe(
+		ofType<SetTheme>(ThemeTypes.SET_THEME),
+		map(a => a.payload),
+		tap(i => {
+			const classList = this.document.body.classList[0] ? this.document.body.classList[0] : 'empty';
+
+			if (i.isDefault) {
+				this.document.body.classList.remove(classList);
+			} else if (classList !== i.name) {
+				this.document.body.classList.remove(classList);
+				this.document.body.classList.add(i.name);
+			}
+		}),
+		tap(x => this.storageService.set(this.key, x)),
+	);
 }
