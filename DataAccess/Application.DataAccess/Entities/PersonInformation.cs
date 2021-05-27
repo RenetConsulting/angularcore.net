e9b8d9119bd5ce3bb5f29582b3e7f568d0dc026f
Andrey Medvedev <andreygasparyan539@gmail.com>
2021-05-27 10:10:39 +0300

    RT1-134 Person information entity added

---
+++ DataAccess/Application.DataAccess/Entities/PersonInformation.cs
@@ -0,0 +1,40 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PersonInformation.cs" company="Renet Consulting, Inc">
+// Copyright (c) Renet Consulting, Inc. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+
+namespace Application.DataAccess.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class PersonInformation
+    {
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int PersonId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public string Region { get; set; }
+
+        public string Country { get; set; }
+    }
+}
---
+++ UI/Application/Migrations/20210527070159_AddedPersonTable.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Application.Migrations
+{
+    public partial class AddedPersonTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
